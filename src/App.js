3885869037e431cf4392ccff446368f99688d803
Divy <divyparekh1810@gmail.com>
2022-06-19 22:56:46 +0530

    Done with Login Page UI

--- src/App.js
+++ src/App.js
@@ -6,15 +6,16 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from './components/header/Header';
 import Home from './components/home/Home';
 import Checkout from './components/checkout/Checkout';
+import Login from './components/login/Login';
 const App = () => {
   return (
     //BEM
     <div className="app">
     <Router>
-      <Header />
       <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/checkout" element={<Checkout />} exact/>
+        <Route path="/" element={<><Header/><Home/></>}/>
+        <Route path="/login" element={<Login />}/>
+        <Route path="/checkout" element={<><Header/><Checkout/></>} exact/>
       </Routes>
     </Router>
   </div>
