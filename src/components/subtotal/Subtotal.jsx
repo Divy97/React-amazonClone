7186cd868f814bb72244004c5e37422a3d6a7c6e
Divy <divyparekh1810@gmail.com>
2022-06-19 22:10:27 +0530

    Done with remove from cart function

--- src/components/subtotal/Subtotal.jsx
+++ src/components/subtotal/Subtotal.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
 import './Subtotal.css';
-import CurrencyFormat from "react-currency-format";
-
+import CurrencyFormat from "react-currency-format"; 
+import { useStateValue } from '../../context/StateProvider';
+import { getBasketTotal } from "../../context/reducer.js";
 
 const Subtotal = () => {
+
+  const [{basket}, dispatch] = useStateValue();
+
   return (
     <div className='subtotal'>
         <CurrencyFormat
         renderText={(value) => (
           <>
             <p>
-              {/* Part of the homework */}
-              Subtotal (0 items): <strong>{0}</strong>
+              Subtotal: ({basket?.length} items): <strong>{value}</strong>
             </p>
             <small className="subtotal__gift">
               <input type="checkbox" /> This order contains a gift
@@ -19,8 +22,8 @@ const Subtotal = () => {
           </>
         )}
         decimalScale={2}
-        value={0}
-        displayType={"text"} //TODO:
+        value={getBasketTotal(basket)}
+        displayType={"text"}
         thousandSeparator={true}
         prefix={"$"}
       />
